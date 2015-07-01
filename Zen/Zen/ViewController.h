ee205d0f13849ebd83befa3e1f035ff5f17c4213
Hyder SM <hydersmh@gmail.com>
2015-07-01 03:25:35 -0400

    Initial Xcode commit

---
+++ Zen/Zen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Zen
+//
+//  Created by Hyder SM on 2015-07-01.
+//  Copyright (c) 2015 Hyder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Zen/Zen/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Zen
+//
+//  Created by Hyder SM on 2015-07-01.
+//  Copyright (c) 2015 Hyder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
